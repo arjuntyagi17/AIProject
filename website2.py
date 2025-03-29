e6d145d8b865b2559f8f2c76008a3c8f36c4aac9
Arjun Tyagi <85330574+arjuntyagi17@users.noreply.github.com>
2025-03-29 15:24:13 +0530

    Update website2.py

--- website2.py
+++ website2.py
@@ -8,7 +8,16 @@ from datetime import datetime
 import json
 #import streamlit.secrets
 
-firebaseConfig = json.loads(st.secrets["firebase"])
+firebaseConfig = {
+    "apiKey": st.secrets["firebase"]["apiKey"],
+    "authDomain": st.secrets["firebase"]["authDomain"],
+    "databaseURL": st.secrets["firebase"]["databaseURL"],
+    "projectId": st.secrets["firebase"]["projectId"],
+    "storageBucket": st.secrets["firebase"]["storageBucket"],
+    "messagingSenderId": st.secrets["firebase"]["messagingSenderId"],
+    "appId": st.secrets["firebase"]["appId"],
+    "measurementId": st.secrets["firebase"]["measurementId"],
+}
 
 # 🔥 Firebase Configuration
 # firebaseConfig = {
